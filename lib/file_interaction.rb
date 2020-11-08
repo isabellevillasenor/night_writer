d5e2334fecf403c6bca989f211c6bf9a8663aa39
Isabelle Villasenor <villasenori@outlook.com>
2020-11-08 07:16:48 -0700

    Add File info to night_writer.rb

--- lib/file_interaction.rb
+++ lib/file_interaction.rb
@@ -5,10 +5,5 @@ class FileInteraction
         @file_name = file_name.gets_chomp
 
     end
-    
-    #WHEN GENERATED USER TERMINAL INTERACTION, THE FILE LENGTH IS THE *FIRST* THING YOU GETS.CHOMP, THE *SECOND* WOULD BE THE NAME
-    def create_file_confirmation
-        "Created '#{@file_name}' containing #{@file.length} characters."
-    end
 
 end
