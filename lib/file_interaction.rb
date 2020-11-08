144c976ab60861c762a6a4c1e2f2c5b28c7c8530
Isabelle Villasenor <villasenori@outlook.com>
2020-11-07 18:21:54 -0700

    Add create_file_confirmation and associated instance variables

--- lib/file_interaction.rb
+++ lib/file_interaction.rb
@@ -1,4 +1,14 @@
 class FileInteraction
+    attr_reader: file_name
     
+    def initialize()
+        @file_name = 
+
+    end
+    
+    #WHEN GENERATED USER TERMINAL INTERACTION, THE FILE LENGTH IS THE *FIRST* THING YOU GETS.CHOMP, THE *SECOND* WOULD BE THE NAME
+    def create_file_confirmation
+        "Created '#{@file_name}' containing #{@file.length} characters."
+    end
 
 end
