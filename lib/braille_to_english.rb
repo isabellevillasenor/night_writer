18411479cffdaa7af8ae003f0705f535a2ecb914
Isabelle Villasenor <villasenori@outlook.com>
2020-11-10 06:14:28 -0700

    Add set_params method

--- lib/braille_to_english.rb
+++ lib/braille_to_english.rb
@@ -2,44 +2,46 @@ require_relative './dictionary'
 
 class BrailleToEnglish
     include Dictionary
-    
+
     attr_reader :text
     def initialize(text)
         @text = text
     end
       
-    def letter_translate(text)
-       assign_english_to_braille.find_all do |braille, letter|
+    def braille_translate(text)
+       assign_english_to_braille.find_all do |braille, value|
          if braille == text
-         assign_english_to_braille.values_at([braille])
+            assign_english_to_braille.values_at([braille])
          end
         end.flatten.pop
     end
     
     def group_rows(text)
         words = []
-        set_params(text).each_slice(3) do |letter|
+        braille_translate(text).each_slice(3) do |letter|
             words << letter
         end
       words
     end
 
-    # def set_params(text)
-    #     letter_translate(text).each_slice(40).to_a.flatten
-    # end
+    def set_params(text)
+        text.scan(/.{1,40}/)
+    end
 
     def translate(text)
         words = []
         top = []
         middle = []
         bottom = []
-        group_rows(text).map do |letter|
+        set_params(text).collect do |word|
+        group_rows(word).map do |letter|
             top << letter[0]
             middle << letter[1]
             bottom << letter[2]        
             words = top + ["\n"] + middle + ["\n"] + bottom
         end
         words.join
+      end
     end
 
 end
--- test/braille_to_english_test.rb
+++ test/braille_to_english_test.rb
@@ -1,6 +1,4 @@
-require 'Minitest/autorun'
-require 'Minitest/pride'
-require './lib/braille_to_english'
+require './test/test_helper'
 
 class BrailleToEnglishTest < Minitest::Test
     def setup
@@ -14,11 +12,22 @@ class BrailleToEnglishTest < Minitest::Test
         assert_instance_of BrailleToEnglish, @english_file
     end
 
+    def test_it_has_attributes
+        expected = "0.0.00.....00..0....0.....000.00..0.0.....0000.00..0...0.0.......0.000..00.0.0...0..0.0...00...00..0...00.\n00.0.00...0.0..0..0.0.0...000.0....00.000.00...0......000.00..0.0.0..0..0.0.....00....0...0000..0.....0000\n.0..00....00.000....00.0...0.0....00.0.0...00.00.000..000.0...0.000..0...00.000.00...0.0...00.0..0000.00.."
+        assert_equal expected, @english_file.text
+    end
+
+    def test_set_params
+        text = "helloheyhibonjourkonichiwahowareyoudoinggood"
+        expected = ["helloheyhibonjourkonichiwahowareyoudoing", "good"]
+        assert_equal expected, @english_file.set_params(text)
+    end
+
     def test_assign_braille_letter_number_and_special_char
-        assert_equal "a", @english_file.letter_translate(["0.", "..", ".."])
+        assert_equal "a", @english_file.braille_translate(["0.", "..", ".."])
         # if you have more time figure out if you could incorporate this to letter translate so you can run whole translate of entire file
         #assert_equal [".0", "0.", ".."], @english_file.assign_num_to_braille["9"]
-        assert_equal "?", @english_file.letter_translate(["..", "0.", "00"])
+        assert_equal "?", @english_file.braille_translate(["..", "0.", "00"])
     end
 
 end
